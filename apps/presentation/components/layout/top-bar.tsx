2ac292a6e7864f81568fa386974b6ee5c20de97c
Arnimag <arnimag@gmail.com>
2026-04-13 12:09:28 +0200

    feat: remove usage of data source indicator in top bar

--- apps/presentation/components/layout/top-bar.tsx
+++ apps/presentation/components/layout/top-bar.tsx
@@ -1,5 +1,4 @@
 import { cn } from '../../lib/utils'
-import { DataSourceIndicator } from '@demo/data-source-selector'
 import { StandardContainer } from '../ui/standard-container'
 import { getTranslations } from 'next-intl/server'
 
@@ -20,7 +19,6 @@ export async function TopBar({ className, messages }: TopBarProps) {
       aria-label={t('ariaLabel')}
       className={cn('sticky top-0 z-50 w-full bg-gray-950 py-1.5', className)}
     >
-      <DataSourceIndicator />
       <StandardContainer className='flex items-center justify-center'>
         <div className='flex flex-wrap items-center justify-center gap-x-6 gap-y-1 text-center text-xs font-normal text-white'>
           {messages.map((msg, i) => (
